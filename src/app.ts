5a4cf2733ac1fc5b8f6d63f2583f6d32b5193a44
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-11-24 14:28:03 +0530

    feat:"create ToDoListify app"

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import connectDB from "./config/db";
+import cookieParser from "cookie-parser";
+import cors, { CorsOptions } from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cookieParser());
+
+const allowedOrigins: string[] = (process.env.ALLOWED_ORIGIN || "")
+  .split(",")
+  .map((origin) => origin.trim());
+
+const corsOptions: CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (allowedOrigins.includes(origin as string) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to ToDoListify application!");
+});
+
+export default app;
